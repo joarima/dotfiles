4871233a5521fb7bfe643f6554fe2f2bf22c8d80
JO ARIMA <joea@JO-no-MacBook-Air.local>
2012-10-23 15:12:55 +0900

    jaspace-mode -> whitespace-mode

--- init.el
+++ init.el
@@ -194,25 +194,54 @@
 ;jaspace
 ;================================================================
 ;(add-to-list 'load-path "~/.emacs.d")
-(require 'jaspace)
+;(require 'jaspace)
 
 ;; 全角空白を表示させる
 
-(setq jaspace-alternate-jaspace-string "□")
+;(setq jaspace-alternate-jaspace-string "□")
 
 ;; 改行記号を表示させる
 
-(setq jaspace-alternate-eol-string "↓\n")
+;(setq jaspace-alternate-eol-string "↓\n")
 
 ;; タブを表示
 
-(setq jaspace-highlight-tabs t)
+;(setq jaspace-highlight-tabs t)
 
 ;; フック
 
-(add-hook 'text-mode-hook 'jaspace-mode)
+;(add-hook 'yatex-mode-hook 'jaspace-mode)
 
-;================================================================
+(when (and (>= emacs-major-version 23)
+	   (require 'whitespace nil t))
+  (setq whitespace-style
+	'(face
+	  tabs spaces newline trailing space-before-tab space-after-tab
+	  space-mark tab-mark newline-mark))
+  (let ((dark (eq 'dark (frame-parameter nil 'background-mode))))
+    (set-face-attribute 'whitespace-space nil
+			:foreground (if dark "pink4" "azure3")
+			:background 'unspecified)
+    (set-face-attribute 'whitespace-tab nil
+			:foreground (if dark "gray20" "gray80")
+			:background 'unspecified
+			:strike-through t)
+    (set-face-attribute 'whitespace-newline nil
+			:foreground (if dark "darkcyan" "darkseagreen")))
+  (setq whitespace-space-regexp "\\(　+\\)")
+  (set-face-bold-p 'whitespace-space t)
+  (setq whitespace-display-mappings
+	'((space-mark   ?\xA0  [?\xA4]  [?_]) ; hard space - currency
+	  (space-mark   ?\x8A0 [?\x8A4] [?_]) ; hard space - currency
+	  (space-mark   ?\x920 [?\x924] [?_]) ; hard space - currency
+	  (space-mark   ?\xE20 [?\xE24] [?_]) ; hard space - currency
+	  (space-mark   ?\xF20 [?\xF24] [?_]) ; hard space - currency
+	  (space-mark   ?　    [?□]    [?＿]) ; full-width space - square
+	  (newline-mark ?\n    [?\xAB ?\n])   ; eol - right quote mark
+	  ))
+  (setq whitespace-global-modes '(not dired-mode tar-mode))
+  (global-whitespace-mode 1))
+  
 ; YaTeX 
 ;================================================================
 ;; YaTeX-mode
